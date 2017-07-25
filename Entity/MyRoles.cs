85b59ce9e9c170172e03d26bd0cf104456d48de9
王成飞 <764297968@qq.com>
2017-07-25 18:31:13 +0800

    fun

--- Entity/MyRoles.cs
+++ Entity/MyRoles.cs
@@ -24,10 +24,8 @@ namespace Entity
         public string Name { get; set; }
         public string Description { get; set; }
         public int Status { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<UserRole> UserRole { get; set; }
-        public virtual ICollection<RoleFunction> RoleFunction { get; set; }
-
     }
 }
