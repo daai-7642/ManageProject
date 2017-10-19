2b661b1ff48458d22ad1628e3e8086ff7a97ef6b
王成飞 <764297968@qq.com>
2017-10-19 13:09:04 +0800

    1

--- OperateForm/GenerateTwoCode.Designer.cs
+++ OperateForm/GenerateTwoCode.Designer.cs
@@ -36,13 +36,14 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.保存路径ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // generate
             // 
-            this.generate.Location = new System.Drawing.Point(377, 109);
+            this.generate.Location = new System.Drawing.Point(464, 110);
             this.generate.Name = "generate";
             this.generate.Size = new System.Drawing.Size(75, 23);
             this.generate.TabIndex = 0;
@@ -52,12 +53,13 @@
             // 
             // browse
             // 
-            this.browse.Location = new System.Drawing.Point(377, 45);
+            this.browse.Location = new System.Drawing.Point(464, 46);
             this.browse.Name = "browse";
             this.browse.Size = new System.Drawing.Size(75, 23);
             this.browse.TabIndex = 1;
             this.browse.Text = "浏览";
             this.browse.UseVisualStyleBackColor = true;
+            this.browse.Visible = false;
             this.browse.Click += new System.EventHandler(this.browse_Click);
             // 
             // fileurl
@@ -75,11 +77,12 @@
             // 
             // content
             // 
-            this.content.Location = new System.Drawing.Point(41, 74);
+            this.content.Location = new System.Drawing.Point(107, 48);
             this.content.Multiline = true;
             this.content.Name = "content";
-            this.content.Size = new System.Drawing.Size(279, 124);
+            this.content.Size = new System.Drawing.Size(295, 124);
             this.content.TabIndex = 3;
+            this.content.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.content_KeyPress);
             // 
             // menuStrip1
             // 
@@ -102,15 +105,25 @@
             // 保存路径ToolStripMenuItem
             // 
             this.保存路径ToolStripMenuItem.Name = "保存路径ToolStripMenuItem";
-            this.保存路径ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.保存路径ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.保存路径ToolStripMenuItem.Text = "保存路径";
             this.保存路径ToolStripMenuItem.Click += new System.EventHandler(this.保存路径ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "生成内容";
             // 
             // GenerateTwoCode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(609, 261);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.content);
             this.Controls.Add(this.fileurl);
             this.Controls.Add(this.browse);
@@ -138,5 +151,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 保存路径ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
     }
 }
--- OperateForm/GenerateTwoCode.cs
+++ OperateForm/GenerateTwoCode.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 using Utility;
 using Utility.Cache;
@@ -39,17 +40,35 @@ namespace OperateForm
         {
             try
             {
-                var list = content.Text.Split(',');
-                for (int i = 0; i < list.Length; i++)
+                if (cache.Get("SavePath") == null)
                 {
-                    Image img = GenerateTwoCodeHelper.Create_ImgCode(list[i], 100);
-                    img.Save(cache.Get("SavePath").ToString() + "/" + list[i] + ".jpg", System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("请选择保存路径");
+                }
+                else
+                {
+                    if (content.Text.Length > 0)
+                    {
+                        var list = content.Text.Split(',');
 
+                        for (int i = 0; i < list.Length; i++)
+                        {
+                            if(!string.IsNullOrWhiteSpace(list[i]))
+                            {
+                                Image img = GenerateTwoCodeHelper.Create_ImgCode(list[i], 100);
+                                img.Save(cache.Get("SavePath").ToString() + "/" + HttpUtility.UrlEncode(list[i]) + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                            }
+                        }
+                        MessageBox.Show("生成完毕");
+                    }
+                    else
+                    {
+                        MessageBox.Show("请填写输出内容");
+                    }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message+ex.StackTrace);
+                MessageBox.Show(ex.Message + ex.StackTrace);
             }
         }
 
@@ -58,7 +77,19 @@ namespace OperateForm
             FolderBrowserDialog pathdlg = new FolderBrowserDialog();
             if (pathdlg.ShowDialog() == DialogResult.OK)
             {
-                cache.AddPermanent("SavePath",pathdlg.SelectedPath);
+                cache.AddPermanent("SavePath", pathdlg.SelectedPath);
+            }
+        }
+        //全选
+        private void content_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox == null)
+                return;
+            if (e.KeyChar == (char)1)       // Ctrl-A 相当于输入了AscII=1的控制字符
+            {
+                textBox.SelectAll();
+                e.Handled = true;      // 不再发出“噔”的声音
             }
         }
     }
